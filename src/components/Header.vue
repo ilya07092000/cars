cb0234e7ca631de8f918ba3597abc834f6613fa0
ilya07092000 <ischenko2012@gmail.com>
2020-07-20 17:04:10 +0300

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="app">
+        <header>
+            <div class="header__title">
+                Car Shop
+            </div>
+            <nav>
+                <ul class="nav__list">
+                    <li class="nav__list__item">
+                        Mercedes-Benz
+                    </li class="nav__list__item">
+                    <li class="nav__list__item">
+                        Bmw
+                    </li>
+                    <li class="nav__list__item">
+                        Tesla
+                    </li>
+                    <li class="nav__list__item">
+                        Rolls-Royce
+                    </li>
+                    <li class="nav__list__item">
+                        Land Rover
+                    </li>
+                </ul>
+            </nav>
+            <div class="header__basket">
+                
+            </div>
+        </header>
+    </div>
+</template>
---
+++ src/views/Landing.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="app">
+        <app-header></app-header>
+        <div class="intro">
+
+        </div>
+    </div>
+</template>
+
+<script>
+import Header from '../components/Header'
+
+export default {
+    components: {
+        appHeader: Header,
+    }
+}
+</script>
+
+<style scoped>
+ .intro {
+    min-height: 700px;
+    width: 100%;
+ }
+</style>
